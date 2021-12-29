19cc986740452593d0f41f94d916ad8548383e8d
unknown <haidawng205@gmail.com>
2021-12-29 21:27:13 +0700

    up db lan dau, acc,prod,receipt

---
+++ cnpm.sql
@@ -0,0 +1,32 @@
+﻿create database cnpm
+use cnpm
+drop database cnpm
+--CREATE TABLE
+--accountant
+create table accountant
+(
+	acc_username NVARCHAR (20)PRIMARY KEY,
+	acc_pass NVARCHAR(20),
+)
+drop table accountant
+INSERT INTO accountant VALUES ('admin123','admin123');
+select * from accountant
+
+
+--Goods received
+create table receipt
+(
+	receipt_id NVARCHAR(20) not null PRIMARY KEY,
+	receipt_name NVARCHAR(20) not null,
+)
+
+
+--Product
+create table product
+(
+	prod_id NVARCHAR(20) not null PRIMARY KEY,
+	prod_name NVARCHAR(20) not null,
+	prod_unit NVARCHAR(20) not null,
+	prod_price INT not null,
+	prod_belong NVARCHAR(20)not null FOREIGN KEY REFERENCES receipt(receipt_id),
+)
